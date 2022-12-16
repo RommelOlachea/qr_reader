864248cd2c9c0487b8df36b4e8b331d1d901140f
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2022-12-16 14:38:49 -0700

    Cambiar la opcion del menu

--- lib/widgets/custom_navigationbar.dart
+++ lib/widgets/custom_navigationbar.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/ui_provider.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 0;
+    final uiProvider = Provider.of<UiProvider>(context);
+    final currentIndex = uiProvider.selectMenuOpt;
 
     return BottomNavigationBar(
-        elevation: 0,
+        onTap: (int i) => uiProvider.selectMenuOpt = i,
         currentIndex: currentIndex,
+        elevation: 0,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.map), label: 'Mapa'),
           BottomNavigationBarItem(
