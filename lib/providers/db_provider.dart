a8a3413bcd7ef8c71e0b72a525b14203493a31c5
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2022-12-20 12:40:22 -0700

    estructu basica del singleton

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -5,6 +5,14 @@ class DBProvider {
   //creamos la instancia, el ._ es un constructor privado,
   //esto nos sirve para que siempre que creemos una nueva instancia,
   //obtengamos la misma instancia de la base de datos
-  static final DBProvider db = DBProvider._();
+  static final DBProvider _db = DBProvider._();
   DBProvider._();
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await initDB();
+    return _database;
+  }
+
+  Future<Database> initDB() async {}
 }
