304624eea215991afa737220d42f77152eada25b
Rommel Olachea <msc.rommel.olachea@gmail.com>
2022-12-21 17:06:35 -0700

    Inicializacion de la base y creacion de la tabla

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -1,17 +1,18 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DBProvider {
-  static Database _database;
+  static Database? _database;
   //creamos la instancia, el ._ es un constructor privado,
   //esto nos sirve para que siempre que creemos una nueva instancia,
   //obtengamos la misma instancia de la base de datos
   static final DBProvider db = DBProvider._();
   DBProvider._();
 
-  Future<Database> get database async {
+  Future<Database?> get database async {
     if (_database != null) return _database;
     _database = await initDB();
     return _database;
