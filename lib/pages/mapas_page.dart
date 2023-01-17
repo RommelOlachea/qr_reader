c2b9bebef9338644d044dbd509e0c1e532feb568
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2023-01-17 14:50:33 -0700

    Mostrar scans en UI sin Base de datos

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -5,8 +5,20 @@ class MapasPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mapas Page'),
-    );
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (_, i) => ListTile(
+              leading: Icon(
+                Icons.map,
+                color: Theme.of(context).primaryColor,
+              ),
+              title: Text('http://asdczcadasdasdasd'),
+              subtitle: Text('ID: 1'),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.grey,
+              ),
+              onTap: () => print('abrir algo....'),
+            ));
   }
 }
